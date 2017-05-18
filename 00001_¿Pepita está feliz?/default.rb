2551da8a382367c8304e89f5b527b101f020e8bb
federico.aloi@gmail.com <federico.aloi@gmail.com>
2017-05-17 21:07:57 -0400

    Mumuki Export on 2017-05-17 21:07:57 -0400

---
+++ 00001_¿Pepita está feliz?/default.rb
@@ -0,0 +1,37 @@
+module Pepita
+  @energia = 1000
+  @ciudad = Obera
+
+  def self.energia
+    @energia 
+  end
+
+  def self.ciudad
+    @ciudad
+  end
+
+  def self.comer_lombriz!
+    @energia += 20
+  end
+
+  def self.volar_en_circulos!
+    @energia -= 10
+  end
+
+  def self.volar_hacia!(destino)
+    self.gastar_energia!(destino)
+    @ciudad = destino
+  end
+
+  def self.gastar_energia!(destino)
+     @energia -= @ciudad.distancia_a(destino) / 2
+  end
+  
+  def self.debil?
+    # completá acá...
+  end
+  
+  def self.feliz?
+    # completá acá...
+  end
+end
---
+++ 00001_¿Pepita está feliz?/description.md
@@ -0,0 +1,10 @@
+¿Te acordás de `Pepita`? Bueno, aunque no lo creas, también cambia de estados de ánimo.
+
+En nuestro **modelo** de Pepita, vamos a representar simplemente dos estados posibles: cuando está débil y cuando está feliz. 
+
+¿Y cuándo ocurre eso? 
+
+* Pepita está _débil_ si su energía es menos de 100. :fearful:
+* Pepita está _feliz_ si su energía es más de 1000. :grin:
+
+> Completá los métodos `debil?` y `feliz?`.
