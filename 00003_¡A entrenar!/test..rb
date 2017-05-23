669a0f5e77e4105b2329e6b75a835a0a40df2262
federico.aloi@gmail.com <federico.aloi@gmail.com>
2017-05-23 18:46:41 -0400

    Mumuki Export on 2017-05-23 18:46:41 -0400

--- 00003_¡A entrenar!/test..rb
+++ 00003_¡A entrenar!/test..rb
@@ -3,12 +3,12 @@ describe 'Pachorra' do
     expect(Pachorra).to be
   end
   
-  it 'entiende el mensaje entrenar_golondrina!' do
-    expect(Pachorra).to respond_to :entrenar_golondrina!
+  it 'entiende el mensaje entrenar_ave!' do
+    expect(Pachorra).to respond_to :entrenar_ave!
   end  
   
-  it 'hace entrenar a Pepita cuando recibe entrenar_golondrina!' do
-    Pachorra.entrenar_golondrina!
+  it 'hace entrenar a Pepita cuando recibe entrenar_ave!' do
+    Pachorra.entrenar_ave!
     expect(Pepita.energia).to eq 1250
   end
 end
