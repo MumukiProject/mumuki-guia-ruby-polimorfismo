ec7da773423db943d3fe86f1d3e6474d7c822140
federico.aloi@gmail.com <federico.aloi@gmail.com>
2017-05-26 14:16:52 -0400

    Mumuki Export on 2017-05-26 14:16:52 -0400

--- 00007_Un entrenamiento más duro/test..rb
+++ 00007_Un entrenamiento más duro/test..rb
@@ -1,7 +1,29 @@
-describe 'Pachorra' do
-  it 'puede entrenar a Norita' do
-    Pachorra.firmar_contrato!(Norita)
-    Pachorra.entrenar_ave!
-    expect(Norita.energia).to eq 120
+describe 'Norita' do
+  it '¿puede entrenar con Pachorra?' do
+    expect(norita_puede_entrenar_con_pachorra).to be false
   end
+  
+  it '¿puede entrenar con Emilce?' do
+    expect(norita_puede_entrenar_con_emilce).to be true
+  end  
+end
+
+describe 'Pepita' do
+  it '¿puede entrenar con Pachorra?' do
+    expect(pepita_puede_entrenar_con_pachorra).to be true
+  end
+  
+  it '¿puede entrenar con Emilce?' do
+    expect(pepita_puede_entrenar_con_emilce).to be true
+  end  
+end
+
+describe 'Pepo' do
+  it '¿puede entrenar con Pachorra?' do
+    expect(pepo_puede_entrenar_con_pachorra).to be true
+  end
+  
+  it '¿puede entrenar con Emilce?' do
+    expect(pepo_puede_entrenar_con_emilce).to be true
+  end  
 end
