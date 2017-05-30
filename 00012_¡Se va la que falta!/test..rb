130e85d20be7f0751c692bb1312df749c621409a
federico.aloi@gmail.com <federico.aloi@gmail.com>
2017-05-30 16:38:12 -0400

    Mumuki Export on 2017-05-30 16:38:12 -0400

--- 00012_¡Se va la que falta!/test..rb
+++ 00012_¡Se va la que falta!/test..rb
@@ -10,49 +10,51 @@ module Mendieta
   end
 end
 
-before(:each) do
-  Eulogia.reiniciar!
-  Mendieta.reiniciar!
-end
-
-describe 'Inodoro' do
-  it 'puede decirnos su cafeína en sangre' do
-    expect(Inodoro.cafeina_en_sangre).to eq 90
+context '' do
+  before(:each) do
+    Eulogia.reiniciar!
+    Mendieta.reiniciar!
   end
   
-  it 'puede tomar mate con Eulogia' do
-    Inodoro.compinche = Eulogia
-    expect(Eulogia).to receive :recibir_mate!
-    Inodoro.tomar_mate!
+  describe 'Inodoro' do
+    it 'puede decirnos su cafeína en sangre' do
+      expect(Inodoro.cafeina_en_sangre).to eq 90
+    end
+    
+    it 'puede tomar mate con Eulogia' do
+      Inodoro.compinche = Eulogia
+      expect(Eulogia).to receive :recibir_mate!
+      Inodoro.tomar_mate!
+    end
+    
+    it 'puede tomar mate con Mendieta' do
+      Inodoro.compinche = Mendieta
+      expect(Mendieta).to receive :recibir_mate!
+      Inodoro.tomar_mate!
+    end  
   end
   
-  it 'puede tomar mate con Mendieta' do
-    Inodoro.compinche = Mendieta
-    expect(Mendieta).to receive :recibir_mate!
-    Inodoro.tomar_mate!
-  end  
-end
-
-describe 'Eulogia' do
-  it 'puede decirnos si está enojada' do
-    expect(Eulogia.enojada).to be false
+  describe 'Eulogia' do
+    it 'puede decirnos si está enojada' do
+      expect(Eulogia.enojada).to be false
+    end
+    
+    it 'se enoja cuando recibe un mate de Inodoro' do
+      Inodoro.compinche = Eulogia
+      Inodoro.tomar_mate!
+      expect(Eulogia.enojada).to be true
+    end
   end
   
-  it 'se enoja cuando recibe un mate de Inodoro' do
-    Inodoro.compinche = Eulogia
-    Inodoro.tomar_mate!
-    expect(Eulogia.enojada).to be true
-  end
-end
-
-describe 'Mendieta' do
-  it 'puede decirnos cuántas ganas de hablar tiene' do
-    expect(Mendieta.ganas_de_hablar).to eq 5
-  end
-  
-  it 'deja de tener ganas de hablar cuando recibe un mate de Inodoro' do
-    Inodoro.compinche = Mendieta
-    Inodoro.tomar_mate!
-    expect(Mendieta.ganas_de_hablar).to eq 0
+  describe 'Mendieta' do
+    it 'puede decirnos cuántas ganas de hablar tiene' do
+      expect(Mendieta.ganas_de_hablar).to eq 5
+    end
+    
+    it 'deja de tener ganas de hablar cuando recibe un mate de Inodoro' do
+      Inodoro.compinche = Mendieta
+      Inodoro.tomar_mate!
+      expect(Mendieta.ganas_de_hablar).to eq 0
+    end
   end
 end
