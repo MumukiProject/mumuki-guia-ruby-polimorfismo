8ade8c9ab08b63631e38f7113b68c2ce3a95c72d
federico.aloi@gmail.com <federico.aloi@gmail.com>
2017-05-30 16:32:02 -0400

    Mumuki Export on 2017-05-30 16:32:02 -0400

--- 00012_¡Se va la que falta!/test..rb
+++ 00012_¡Se va la que falta!/test..rb
@@ -1,2 +1,11 @@
 describe 'Inodoro' do
+  it 'puede decirnos su cafeína en sangre' do
+    expect(Inodoro.cafeina_en_sangre).to eq 90
+  end
+  
+  it 'puede tomar mate con Eulogia' do
+    Inodoro.compinche = Eulogia
+    expect(Eulogia).to receive :recibir_mate!
+    Inodoro.tomar_mate!
+  end  
 end
