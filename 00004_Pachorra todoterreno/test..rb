f1ee357b8fb2d23a10a53caf015542b9174014d9
federico.aloi@gmail.com <federico.aloi@gmail.com>
2017-06-02 17:35:22 -0400

    Mumuki Export on 2017-06-02 17:35:21 -0400

--- 00004_Pachorra todoterreno/test..rb
+++ 00004_Pachorra todoterreno/test..rb
@@ -6,12 +6,12 @@ describe 'Pachorra' do
   it 'firma contrato con Pepita y la entrena' do
     Pachorra.firmar_contrato!(Pepita)
     Pachorra.entrenar_ave!
-    expect(Pepita.energia).to eq 1030
+    expect(Pepita.energia).to eq 1250
   end
   
   it 'firma contrato con Pepo y lo entrena' do
     Pachorra.firmar_contrato!(Pepo)
     Pachorra.entrenar_ave!
-    expect(Pepo.energia).to eq 1030
+    expect(Pepo.energia).to eq 1000
   end  
 end
